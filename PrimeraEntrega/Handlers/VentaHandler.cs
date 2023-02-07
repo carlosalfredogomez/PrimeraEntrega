4b08bd0ffa4e8de3dc34fca1470462a8e476716b
carlos.alfredo.gomez <carlos.alfredo.gomez@gmail.com>
2023-02-07 20:57:28 -0300

    Agregar archivos de proyecto.

---
+++ PrimeraEntrega/Handlers/VentaHandler.cs
@@ -0,0 +1,43 @@
+﻿using PrimeraEntrega.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraEntrega.Handlers
+{
+    public class VentaHandler
+    {
+        public static string connetionString = "Data Source=QT5\\SQLEXPRESS;Initial Catalog=SistemaGestion;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static List<Venta> ObtenerVentas(long idUsuario)
+        {
+            List<Venta> ventas = new List<Venta>();
+
+            using (SqlConnection conn = new SqlConnection(connetionString))
+            {
+                SqlCommand comando = new SqlCommand("SELECT * FROM Venta WHERE @IdUsuario = idUsuario", conn);
+                comando.Parameters.AddWithValue("@IdUsuario", idUsuario);
+                conn.Open();
+
+                SqlDataReader reader = comando.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Venta ventaTemp = new Venta();
+                        ventaTemp.Id = reader.GetInt64(0);
+                        ventaTemp.Comentarios = reader.GetString(1);
+                        ventaTemp.IdUsuario = reader.GetInt64(2);
+                        ventas.Add(ventaTemp);
+                    }
+                }
+
+                return ventas;
+
+            }
+
+        }
+    }
+}
---
+++ PrimeraEntrega/Program.cs
@@ -0,0 +1,55 @@
+﻿using PrimeraEntrega.Handlers;
+using PrimeraEntrega.Models;
+
+namespace PrimeraEntrega
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Traer Usuario 
+            Usuario usuario = UsuarioHandler.ObtenerUsuario(2);
+            Console.WriteLine($"Usuario Id: {usuario.Id}, Usuario: {usuario.Nombre}");
+
+            Console.WriteLine("   ");
+
+            // Traer Productos
+            List<Producto> productos = ProductoHandler.ObtenerProductos(1);
+            foreach (var item in productos)
+            {
+                Console.WriteLine($"Productos cargados por el usuario {item.IdUsuario}: {item.Descripciones}");
+            }
+
+            Console.WriteLine("   ");
+            Console.WriteLine(" Produtos vendidos ");
+
+            // Traer Produtos vendidos
+            List<Producto> productosVendidos = ProductoHandler.ObtenerProductoVendido(1);
+            foreach (var item in productosVendidos)
+            {
+                Console.WriteLine($"Los Productos Vendidos por Usuario Id {item.IdUsuario}: {item.Descripciones}");
+            }
+
+            Console.WriteLine("    ");
+            Console.WriteLine("  Ventas  ");
+
+            // Traer Ventas 
+            List<Venta> ventas = VentaHandler.ObtenerVentas(1);
+            foreach (var item in ventas)
+            {
+                Console.WriteLine($"Ventas Usuario: {item.IdUsuario}, Id de la Venta: {item.Id}");
+            }
+
+            Console.WriteLine("   ");
+            
+            // Inicio de sesión 
+            Usuario usuarioLogin = UsuarioHandler.ObtenerUsuarioLogin("eperez", "SoyErnestoPerez");
+            Console.WriteLine($" Inicio de Sesión --- Usuario: {usuarioLogin.Nombre} \t  Apellido: {usuarioLogin.Apellido} \tMail: {usuarioLogin.Mail}");
+           
+
+
+
+        }
+    }
+}
